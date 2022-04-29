e0525c5360cd49c4645e024ed9ce9f6efdc81984
FriesSec <christian.fries123@gmail.com>
2022-04-29 08:47:03 +0200

    cli: use poetry and update dependencies

--- oidc-lab-scripts/cli.py
+++ oidc-lab-scripts/cli.py
@@ -4,7 +4,7 @@ import os
 import sys
 import argparse
 import cmd2
-from cmd2 import style, fg, with_argparser, with_category
+from cmd2 import style, Fg, with_argparser, with_category
 
 from api.op import OpTest
 from api.rp import RpTest
@@ -21,13 +21,13 @@ class Cli(cmd2.Cmd):
         shortcuts = dict(cmd2.DEFAULT_SHORTCUTS)
         shortcuts.update({'ll': 'list', 'ls': 'list'})
 
-        super().__init__(use_ipython=True,
+        super().__init__(include_ipy=True,
                          multiline_commands=['orate'],
                          persistent_history_file='history.dat',
                          persistent_history_length=1000,
                          shortcuts=shortcuts)
 
-        self.intro = style('Starting Control Center for Professos!', fg=fg.green, bold=True)
+        self.intro = style('Starting Control Center for Professos!', fg=Fg.GREEN, bold=True)
         self.prompt = 'cli> '
         self.default_category = 'Others'
 
@@ -45,7 +45,7 @@ class Cli(cmd2.Cmd):
         self.poutput("\nAvailable configurations:")
         self.poutput("==========================\n")
         for cfg in os.listdir(cfg_path):
-            self.poutput(cmd2.style(cfg, fg=cmd2.fg.green))
+            self.poutput(cmd2.style(cfg, fg=cmd2.Fg.GREEN))
 
     load_parser = cmd2.Cmd2ArgumentParser('load')
     load_parser.add_argument('target', choices=['op', 'rp'])
